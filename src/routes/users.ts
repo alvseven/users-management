16a1855ac09c1bc3bca631c48b147da819d141f9
alvseven <alv7contato@outlook.com>
2025-04-09 15:18:29 -0300

    feat: users route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
 
+import { usersController } from "../controllers/users";
+import { verifyToken } from "../middlewares/verify-token";
+
 export const usersRoutes = Router();
 
-usersRoutes.get("/", (_req, res) => {
-	res.json({ message: "Hello,world!" });
-});
+const { create, getAll, login } = usersController();
+
+usersRoutes.get("/", verifyToken, getAll);
+usersRoutes.post("/", create);
+usersRoutes.post("/login", login);
