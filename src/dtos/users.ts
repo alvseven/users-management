d379f69dda926131456c27eeeae4887d432c6ba5
alvseven <alv7contato@outlook.com>
2025-04-09 15:18:41 -0300

    feat: users dto

---
+++ src/dtos/users.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const createUserRequestSchema = z.object({
+	id: z.string(),
+	name: z.string(),
+	email: z.string(),
+	document: z.string().length(11),
+	password: z
+		.string()
+		.min(8, {
+			message: "O campo [password] deve conter no mínimo 8 caracteres",
+		})
+		.regex(
+			/(?=.*?[A-Z])/,
+			"O campo [password] deve conter no mínimo uma letra maiúscula",
+		)
+		.regex(
+			/(?=.*?[a-z])/,
+			"O campo [password] deve conter no mínimo uma letra minúscula",
+		)
+		.regex(/(?=.*?\d)/, "O campo [password] deve conter no mínimo um número"),
+});
+export type CreateUserRequest = z.infer<typeof createUserRequestSchema>;
+
+export const userLoginRequestSchema = z.object({
+	email: z.string(),
+	password: z.string(),
+});
+export type userLoginRequest = z.infer<typeof userLoginRequestSchema>;
---
+++ src/dtos/validate-schema.ts
@@ -0,0 +1,32 @@
+import type { ZodSchema } from "zod";
+
+import { zodCustomErrorMap } from "src/config/zod-error-map";
+
+type SuccessParse<T> = {
+	success: true;
+	data: T;
+};
+
+type ErrorParse = {
+	success: false;
+	errors: string[];
+};
+
+export type ParseResult<T> = SuccessParse<T> | ErrorParse;
+
+export const validateSchema = <T>(
+	schema: ZodSchema<T>,
+	data: unknown,
+): ParseResult<T> => {
+	const result = schema.safeParse(data, {
+		errorMap: zodCustomErrorMap,
+	});
+
+	if (result.success) {
+		return { success: true, data: result.data };
+	}
+
+	const errorMessages = result.error.errors.map((error) => error.message);
+
+	return { success: false, errors: errorMessages };
+};
